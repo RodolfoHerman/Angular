446eec7818d52c2ba4cabed799166cbc779a14a6
RodolfoHerman <ciencia.rodolfo@gmail.com>
2018-06-22 15:08:03 -0300

    Tópico 8, Serviço HTTP para Detalhe do Restaurante e o Operador Elvis

--- src/app/restaurant-detalhe/restaurant-detalhe.component.ts
+++ src/app/restaurant-detalhe/restaurant-detalhe.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { RestaurantsService } from '../restaurants/restaurants.service';
+import { Restaurant } from '../restaurants/restaurant/restaurant.model';
 
 @Component({
   selector: 'mt-restaurant-detalhe',
@@ -6,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetalheComponent implements OnInit {
 
-  constructor() { }
+  restaurant: Restaurant;
+  
+  //O 'ActivatedRoute' captura o parâmetro da url para ser passado para a função
+  constructor(private restaurantServive: RestaurantsService,
+              private rout: ActivatedRoute) { }
 
   ngOnInit() {
+
+    //Existe duas forma de capturar o parâmetro da url. Primeira como 'snapshot' que busca o parâmetro naquele momento e a outra é pelo 'subscribe' que fica observando a troca de roa. Como acessaremos essa url uma vez para expor os itens do retaurante não é preciso usar o 'subscribe' (reactive)
+    this.restaurantServive.restaurantById(this.rout.snapshot.params['id'])
+          .subscribe(restaurant => this.restaurant = restaurant);
   }
 
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -21,4 +21,11 @@ export class RestaurantsService {
             .catch(ErrorHandler.handleError);
     }
 
+    restaurantById(id: String): Observable<Restaurant> {
+
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError);
+    }
+
 }
