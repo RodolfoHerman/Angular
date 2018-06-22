27b962f66d993d29648cb06c65aa336b5415b431
RodolfoHerman <ciencia.rodolfo@gmail.com>
2018-06-22 17:07:19 -0300

    Tópico 10, Pipes Asyc e Date, Implementando as Avaliações dos Restaurantes

--- src/app/restaurant-detalhe/reviews/reviews.component.ts
+++ src/app/restaurant-detalhe/reviews/reviews.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
+
+import { RestaurantsService } from '../../restaurants/restaurants.service';
 
 @Component({
   selector: 'mt-reviews',
@@ -6,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  //utiliando o PIPE ASYNC para realizar o subscribe
+  reviews: Observable<any>;
+
+  constructor(private restaurantsService: RestaurantsService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+
+    //Estamos em um componente filho e queremos acessar uma rota filha, para isso recuperamos a activatedRoute do parent e depois pegamos o parâmtro id
+    this.reviews = this.restaurantsService.reviewsOfRestaurant(this.activatedRoute.parent.snapshot.params['id']);
   }
 
 }
