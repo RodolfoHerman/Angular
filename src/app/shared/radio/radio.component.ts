3b5b3d621e76bb6248dfc13bf1a950ba95a9c7c7
RodolfoHerman <ciencia.rodolfo@gmail.com>
2018-06-26 13:47:46 -0300

    Tópico 21, Criando o Componente de Radio Buttons Usando o ControlValueAccessor II

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,24 +1,59 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
 import { RadioOption } from './radio-option.model';
 
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
   @Input() options: RadioOption[];
 
   value: any;
+
+  onChange: any;
   
   constructor() { }
 
   ngOnInit() {
   }
 
-  setValue(value: any) {
+  setValue(value: any): void {
 
     this.value = value;
+
+    //Avisa para as diretivas ques estão utilizando o componente que ele mudou de estado
+    this.onChange(this.value);
+  }
+
+  //Será chamado pelas diretivas quando elas querem passar um valor para o componente
+  writeValue(obj: any): void {
+
+    this.value = obj;
+  }
+
+  //Passa uma função para o componente e essa função terá que ser chamada sempre que o valor interno do componente mudar
+  registerOnChange(fn: any): void {
+
+    this.onChange = fn;
+  }  
+
+  //Se o usuário entrou no componente
+  registerOnTouched(fn: any): void {
+
+  }
+
+  setDisabledState?(disabled: boolean): void {
+
   }
 
 }
