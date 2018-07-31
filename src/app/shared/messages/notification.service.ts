a4535662f1cdcc26eea131858dd6a294d95bcbba
RodolfoHerman <ciencia.rodolfo@gmail.com>
2018-07-31 12:38:52 -0300

    Tópico 38 - Animações, Usando os Operadores 'Do' e 'SwitchMap' Para Controlar o SnackBar

--- src/app/shared/messages/notification.service.ts
+++ src/app/shared/messages/notification.service.ts
@@ -1,9 +1,10 @@
 import { EventEmitter } from "@angular/core";
 
+
 //Criar um SERVICE para que esteja disponível em toda a aplicação
 export class NotificationService {
 
-    notifier = new EventEmitter<String>();
+    notifier = new EventEmitter<string>();
 
     notify(message: string): void {
 
--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -3,6 +3,9 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 import { NotificationService } from '../notification.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
+
 
 @Component({
   selector: 'mt-snackbar',
@@ -10,7 +13,7 @@ import 'rxjs/add/observable/timer';
   styleUrls: ['./snackbar.component.css'],
   animations: [
     trigger('snack-visibility', [
-      state('hiden', style({
+      state('hidden', style({
         opacity: 0,
         bottom: '0px'
       })),
@@ -18,26 +21,28 @@ import 'rxjs/add/observable/timer';
         opacity: 1,
         bottom: '30px'
       })),
-      transition('hiden => visible', animate('500ms 0s ease-in')),
-      transition('visible => hiden', animate('500ms 0s ease-out'))
+      transition('hidden => visible', animate('500ms 0s ease-in')),
+      transition('visible => hidden', animate('500ms 0s ease-out'))
     ])
   ]
 })
 export class SnackbarComponent implements OnInit {
 
-  message: string;
+  message: string = '';
 
-  snackVisibility: string = 'hiden';
+  snackVisibility: string = 'hidden';
 
   constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
 
-    this.notificationService.notifier.subscribe(message => {
-      this.message = message;
-      this.snackVisibility = 'visible';
-      Observable.timer(3000).subscribe(timer => this.snackVisibility = 'hiden');
-    })
+    this.notificationService.notifier
+      .do(message => {
+        this.message = message;
+        this.snackVisibility = 'visible';
+    }).switchMap(message => Observable.timer(3000))
+      .subscribe(timer => this.snackVisibility = 'hidden');
+  
   }
 
 }
